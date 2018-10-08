bdc07445a0b20fd8ff1ab3a912bc40b1ffe99fa9
Madison <madisononealburke@gmail.com>
2018-10-08 12:53:07 +0000

    rearrange api data into json format

---
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Records all tasks from all employees """
+import json
+import requests
+
+base_url = "https://jsonplaceholder.typicode.com/"
+user_req = requests.get("{}{}".format(base_url, "users"))
+users = user_req.json()
+todo_req = requests.get("{}{}".format(base_url, "todos"))
+todos = todo_req.json()
+
+output = {}
+for i in range(len(users)):
+    u = users[i]['id']
+    output[u] = []
+    username = users[i]['name']
+    for j in range(len(todos)):
+        todo = todos[j]
+        title = todo['title']
+        completed = todo['completed']
+        task = {'username': username, 'task': title, 'completed': completed}
+        output[u].append(task)
+
+with open("todo_all_employees.json", mode="w") as fd:
+    json.dump(output, fd)
